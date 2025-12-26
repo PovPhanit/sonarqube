b9945009ea581c200b38a8bead2f6acfa4f12195
PovPhanit <povphanit8@gmail.com>
2025-12-26 15:44:29 +0700

    updaet

---
+++ app.test.js
@@ -0,0 +1,12 @@
+// app.test.js
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /hello', () => {
+  it('should return Hello World', async () => {
+    const res = await request(app).get('/hello');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello World');
+  });
+});
+
